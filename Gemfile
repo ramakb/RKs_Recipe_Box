57c94015e26d0ada21f5b9159ccf71f56b25f3cb
ramakb <rama_kb@hotmail.com>
2015-02-09 14:00:59 +0000

    Recipe MVC create and basic functionality work

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
-
+gem 'haml', '~> 4.0.6'
+gem 'simple_form', '~> 3.1.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
-gem 'haml', '~> 4.0.6'
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,62 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+  # GET /recipes
+  # GET /recipes.json
+  def index
+    @recipes = Recipe.all.order("created_at DESC")
+  end
+
+  # GET /recipes/1
+  # GET /recipes/1.json
+  def show
+  end
+
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+
+  # GET /recipes/1/edit
+  def edit
+  end
+
+  # POST /recipes
+  # POST /recipes.json
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created the new Recipe"
+    else
+      render 'new'
+    end
+  end
+
+  # PATCH/PUT /recipes/1
+  # PATCH/PUT /recipes/1.json
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE /recipes/1
+  # DELETE /recipes/1.json
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted the Recipe"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :user_id)
+  end
+end
